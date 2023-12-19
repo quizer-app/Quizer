f00f95e906308ef631eff00a879636f6146db781
EloToJa <elotoja@protonmail.com>
2023-12-19 16:48:12 +0100

    Implement domain base classes

---
+++ src/Quizer.Domain/Common/Models/AggregateRoot.cs
@@ -0,0 +1,10 @@
+﻿namespace Quizer.Domain.Common.Models
+{
+    public abstract class AggregateRoot<TId> : Entity<TId>
+        where TId : notnull
+    {
+        protected AggregateRoot(TId id) : base(id)
+        {
+        }
+    }
+}
---
+++ src/Quizer.Domain/Common/Models/Entity.cs
@@ -0,0 +1,43 @@
+﻿namespace Quizer.Domain.Common.Models
+{
+    public abstract class Entity<TId> : IEquatable<Entity<TId>>
+        where TId : notnull
+    {
+        public TId Id { get; protected set; }
+
+        protected Entity(TId id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null || obj.GetType() != GetType())
+                return false;
+
+            var entity = (Entity<TId>)obj;
+
+            return Id.Equals(entity.Id);
+        }
+
+        public bool Equals(Entity<TId>? other)
+        {
+            return Equals(other);
+        }
+
+        public static bool operator ==(Entity<TId>? a, Entity<TId>? b)
+        {
+            return Equals(a, b);
+        }
+
+        public static bool operator !=(Entity<TId>? a, Entity<TId>? b)
+        {
+            return !Equals(a, b);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.GetHashCode();
+        }
+    }
+}
